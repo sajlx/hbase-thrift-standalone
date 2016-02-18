43afb13419228aac9041e0259d2dd1a02ce0e915
sajlx <ivan.bettarini@gmail.com>
2016-02-18 15:27:59 +0100

    add external intial data script

--- start-pseudo-distributed-thrift.sh
+++ start-pseudo-distributed-thrift.sh
@@ -1,8 +1,32 @@
 /opt/hbase/bin/hbase zookeeper > logzoo.log 2>&1 &
 /opt/hbase/bin/hbase regionserver start > logregion.log 2>&1 &
 
-/opt/hbase/bin/hbase master start --localRegionServers=0 &
+/opt/hbase/bin/hbase master start --localRegionServers=0 > master.log 2>&1 &
 
-/opt/hbase/bin/hbase thrift start > thrift.log 2>&1
+/opt/hbase/bin/hbase thrift start > thrift.log 2>&1 &
 
 # /opt/hbase/bin/hbase shell /opt/hbase/bin/create_table_hbase.txt
+
+
+DIR=/opt/hbase/bin/initial_script
+TMP_ALL=/*
+
+if [ "$(ls -A $DIR)" ];
+    then
+        echo "$DIR is NOT Empty"
+        sleep 10
+    else
+        echo "$DIR is Empty"
+fi
+
+for FILE_SCRIPT in $DIR$TMP_ALL; do
+    echo "Executing $FILE_SCRIPT hbase script..."
+    /opt/hbase/bin/hbase shell $FILE_SCRIPT
+    echo "Ended executing $FILE_SCRIPT hbase script..."
+    echo
+done
+
+# don't exit from container after script ends
+while :; do
+  sleep 300
+done
