b335d18bfa4f498af3d822cdebdf21cbafd1f460
sajlx <ivan.bettarini@gmail.com>
2016-02-29 11:13:36 +0100

    add a better management of master start

--- start-pseudo-distributed-thrift.sh
+++ start-pseudo-distributed-thrift.sh
@@ -3,7 +3,16 @@
 
 /opt/hbase/bin/hbase master start --localRegionServers=0 > master.log 2>&1 &
 
-sleep 10
+echo 'Checking master status on port 60010 '
+curl localhost:60010
+rc=$?
+while [[ $rc != 0 ]]
+    do
+        echo 'Checking master status on port 60010 result $rc'
+        curl localhost:60010
+        rc=$?
+    done
+echo 'GOOD checking master status on port 60010 $rc'
 
 /opt/hbase/bin/hbase thrift start > thrift.log 2>&1 &
 
@@ -13,26 +22,16 @@ TMP_ALL=/*
 if [ "$(ls -A $DIR)" ];
     then
         echo "$DIR is NOT Empty"
-        echo 'Checking master status on port 60010 '
-        curl localhost:60010
-        rc=$?
-        while [[ $rc != 0 ]]
-            do
-                echo 'Checking master status on port 60010 result $?'
-                curl localhost:60010
-                rc=$?
-            done
-        echo 'GOOD checking master status on port 60010 $?'
+        for FILE_SCRIPT in $DIR$TMP_ALL; do
+            echo "Executing $FILE_SCRIPT hbase script..."
+            /opt/hbase/bin/hbase shell $FILE_SCRIPT
+            echo "Ended executing $FILE_SCRIPT hbase script..."
+            echo
+        done
     else
         echo "$DIR is Empty"
 fi
 
-for FILE_SCRIPT in $DIR$TMP_ALL; do
-    echo "Executing $FILE_SCRIPT hbase script..."
-    /opt/hbase/bin/hbase shell $FILE_SCRIPT
-    echo "Ended executing $FILE_SCRIPT hbase script..."
-    echo
-done
 
 # don't exit from container after script ends
 while :; do
